c1317e1470604254b54981038863b0f9dd81d531
Priit Laes <plaes@plaes.org>
2016-12-31 14:39:15 +0200

    Reduce differences in sta.h

--- sta.h
+++ sta.h
@@ -1,13 +1,15 @@
 /*
- * sta interfaces for XRadio drivers
+ * Mac80211 STA interface for ST-Ericsson CW1200 mac80211 drivers
  *
+ * Copyright (c) 2010, ST-Ericsson
+ * Author: Dmitry Tarnyagin <dmitry.tarnyagin@lockless.no>
  * Copyright (c) 2013, XRadio
- * Author: XRadio
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
+
 #ifndef STA_H_INCLUDED
 #define STA_H_INCLUDED
 
@@ -32,36 +34,42 @@
 
 int cw1200_start(struct ieee80211_hw *dev);
 void cw1200_stop(struct ieee80211_hw *dev);
-int cw1200_add_interface(struct ieee80211_hw *dev, struct ieee80211_vif *vif);
-void cw1200_remove_interface(struct ieee80211_hw *dev, struct ieee80211_vif *vif);
+int cw1200_add_interface(struct ieee80211_hw *dev,
+			 struct ieee80211_vif *vif);
+void cw1200_remove_interface(struct ieee80211_hw *dev,
+			     struct ieee80211_vif *vif);
 int cw1200_change_interface(struct ieee80211_hw *dev,
-                            struct ieee80211_vif *vif,
-                            enum nl80211_iftype new_type,
-                            bool p2p);
+			    struct ieee80211_vif *vif,
+			    enum nl80211_iftype new_type,
+			    bool p2p);
 int cw1200_config(struct ieee80211_hw *dev, u32 changed);
-int cw1200_change_interface(struct ieee80211_hw *dev,
-                            struct ieee80211_vif *vif,
-                            enum nl80211_iftype new_type,
-                            bool p2p);
 void cw1200_configure_filter(struct ieee80211_hw *dev,
-                             unsigned int changed_flags,
-                             unsigned int *total_flags,
-                             u64 multicast);
+			     unsigned int changed_flags,
+			     unsigned int *total_flags,
+			     u64 multicast);
 int cw1200_conf_tx(struct ieee80211_hw *dev, struct ieee80211_vif *vif,
-                   u16 queue, const struct ieee80211_tx_queue_params *params);
+		   u16 queue, const struct ieee80211_tx_queue_params *params);
 int cw1200_get_stats(struct ieee80211_hw *dev,
-                     struct ieee80211_low_level_stats *stats);
+		     struct ieee80211_low_level_stats *stats);
 /* Not more a part of interface?
 int cw1200_get_tx_stats(struct ieee80211_hw *dev,
 			struct ieee80211_tx_queue_stats *stats);
 */
 int cw1200_set_key(struct ieee80211_hw *dev, enum set_key_cmd cmd,
-                   struct ieee80211_vif *vif, struct ieee80211_sta *sta,
-                   struct ieee80211_key_conf *key);
+		   struct ieee80211_vif *vif, struct ieee80211_sta *sta,
+		   struct ieee80211_key_conf *key);
+
 int cw1200_set_rts_threshold(struct ieee80211_hw *hw, u32 value);
 
-void cw1200_flush(struct ieee80211_hw *hw, bool drop);
+/* TODO: extra parameters in mainline driver */
+void cw1200_flush(struct ieee80211_hw *hw,
+		bool drop);
 
+
+u64 cw1200_prepare_multicast(struct ieee80211_hw *hw,
+			     struct netdev_hw_addr_list *mc_list);
+
+int cw1200_set_pm(struct cw1200_vif *priv, const struct wsm_set_pm *arg);
 
 int cw1200_remain_on_channel(struct ieee80211_hw *hw,
                              struct ieee80211_channel *chan,
@@ -69,9 +77,6 @@ int cw1200_remain_on_channel(struct ieee80211_hw *hw,
                              int duration);
 int cw1200_cancel_remain_on_channel(struct ieee80211_hw *hw);
 int cw1200_set_arpreply(struct ieee80211_hw *hw, struct ieee80211_vif *vif);
-u64 cw1200_prepare_multicast(struct ieee80211_hw *hw,
-                             struct netdev_hw_addr_list *mc_list);
-int cw1200_set_pm(struct cw1200_vif *priv, const struct wsm_set_pm *arg);
 void cw1200_set_data_filter(struct ieee80211_hw *hw,
                             struct ieee80211_vif *vif,
                             void *data,
@@ -87,7 +92,7 @@ void cw1200_channel_switch_cb(struct cw1200_common *hw_priv);
 /* ******************************************************************** */
 /* WSM events								*/
 
-void cw1200_free_event_queue(struct cw1200_common *hw_priv);
+void cw1200_free_event_queue(struct cw1200_common *priv);
 void cw1200_event_handler(struct work_struct *work);
 void cw1200_bss_loss_work(struct work_struct *work);
 void cw1200_connection_loss_work(struct work_struct *work);
@@ -97,21 +102,25 @@ void cw1200_tx_failure_work(struct work_struct *work);
 /* ******************************************************************** */
 /* Internal API								*/
 
-int cw1200_setup_mac(struct cw1200_common *hw_priv);
-void cw1200_join_work(struct work_struct *work);
+int cw1200_setup_mac(struct cw1200_common *priv);
 void cw1200_join_timeout(struct work_struct *work);
 void cw1200_unjoin_work(struct work_struct *work);
 void cw1200_offchannel_work(struct work_struct *work);
 void cw1200_wep_key_work(struct work_struct *work);
+
 void cw1200_update_filtering(struct cw1200_vif *priv);
 void cw1200_update_filtering_work(struct work_struct *work);
-int __cw1200_flush(struct cw1200_common *hw_priv, bool drop, int if_id);
 void cw1200_set_beacon_wakeup_period_work(struct work_struct *work);
 int cw1200_enable_listening(struct cw1200_vif *priv, struct ieee80211_channel *chan);
 int cw1200_disable_listening(struct cw1200_vif *priv);
-int cw1200_set_uapsd_param(struct cw1200_vif *priv, const struct wsm_edca_params *arg);
+int cw1200_set_uapsd_param(struct cw1200_vif *priv,
+				const struct wsm_edca_params *arg);
 void cw1200_ba_work(struct work_struct *work);
 void cw1200_ba_timer(unsigned long arg);
+
+int __cw1200_flush(struct cw1200_common *hw_priv, bool drop, int if_id);
+void cw1200_join_work(struct work_struct *work);
+
 const u8 *cw1200_get_ie(u8 *start, size_t len, u8 ie);
 int cw1200_vif_setup(struct cw1200_vif *priv);
 int cw1200_setup_mac_pvif(struct cw1200_vif *priv);
@@ -134,4 +143,5 @@ int cw1200_tesmode_event(struct wiphy *wiphy, const u32 msg_id,
 int cw1200_get_tx_power_range(struct ieee80211_hw *hw);
 int cw1200_get_tx_power_level(struct ieee80211_hw *hw);
 #endif /* CONFIG_XRADIO_TESTMODE */
+
 #endif
